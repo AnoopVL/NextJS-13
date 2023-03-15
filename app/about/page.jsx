9cbb834617654d656fd984ea065cad1ee2f9bd04
AnoopVL <avlanjekar4@gmail.com>
2023-03-15 19:16:36 +0530

    updates

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,20 @@
-export default function About(){
- //   throw new Error()
+export default async function About(){
+    // const data = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${process.env.API_KEY}`)
+    // const res = await data.json()
+    // console.log(res)
+    // console.log("test")
+    // return(
+    //     <main>
+    //         <h1 className="">This is the about page</h1>
+    //         {res.results.map((movie) => (
+    //             <Movie/>
+    //         ))}
+    //     </main>
+    // )
     return(
         <main>
-            <h1>This is the about page</h1>
+            <h1>This is about page</h1>
         </main>
     )
+
 }
